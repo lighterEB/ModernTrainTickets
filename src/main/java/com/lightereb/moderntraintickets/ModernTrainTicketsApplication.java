a164e2f9fda0dabd8dc4fd6c2c2919cd0bee2685
lighterEB <lightereb@126.com>
2024-11-13 22:37:26 +0800

    初始化

---
+++ src/main/java/com/lightereb/moderntraintickets/ModernTrainTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.lightereb.moderntraintickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModernTrainTicketsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModernTrainTicketsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lightereb/moderntraintickets/ModernTrainTicketsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lightereb.moderntraintickets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModernTrainTicketsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

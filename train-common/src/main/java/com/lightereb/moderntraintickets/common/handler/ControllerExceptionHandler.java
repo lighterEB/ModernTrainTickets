a17d1a5625aec551ee6b6d76b53dac0d9dd5d204
lighterEB <hellohus726@126.com>
2024-12-07 23:33:13 +0800

    feat:增加统一异常处理

---
+++ train-common/src/main/java/com/lightereb/moderntraintickets/common/handler/ControllerExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.lightereb.moderntraintickets.common.handler;
+
+import com.lightereb.moderntraintickets.common.exception.BusinessException;
+import com.lightereb.moderntraintickets.common.resp.Result;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 统一异常处理
+ * author: lighterEB
+ * date: 2024-12-07
+ */
+@ControllerAdvice
+public class ControllerExceptionHandler {
+    private static final Logger LOG = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
+    /**
+     * 系统异常
+     * @param e 系统异常
+     * @return 结果
+     */
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public Result<String> handleException(final Exception e) {
+        LOG.error("系统异常：{}", e.getMessage());
+        return Result.failed(e.getMessage());
+    }
+
+
+    /**
+     * 业务异常
+     * @param e 业务异常
+     * @return 结果
+     */
+    @ExceptionHandler(value = BusinessException.class)
+    @ResponseBody
+    public Result<String> handleException(final BusinessException e) {
+        Object msg =  e.getMessage() == null ? e.getE().getDesc() : e.getMessage();
+        LOG.error("业务异常：{}", msg);
+        return Result.failed(msg.toString());
+    }
+}
